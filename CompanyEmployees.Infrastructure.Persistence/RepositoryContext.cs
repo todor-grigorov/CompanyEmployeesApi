b114332f30d1f1c69e8c15fa8f5c793c5c8b6e68
todor-grigorov <t.grigorov99@gmail.com>
2025-05-26 23:29:03 +0200

    feat: add context class

---
+++ CompanyEmployees.Infrastructure.Persistence/RepositoryContext.cs
@@ -0,0 +1,20 @@
+﻿using CompanyEmployees.Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Infrastructure.Persistence
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options)
+        : base(options)
+        {
+        }
+        public DbSet<Company>? Companies { get; set; }
+        public DbSet<Employee>? Employees { get; set; }
+    }
+}
