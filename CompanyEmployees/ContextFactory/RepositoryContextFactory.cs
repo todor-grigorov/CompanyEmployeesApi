c12244f0cbc2a75a023f14efdf64eed4a91fe550
todor-grigorov <t.grigorov99@gmail.com>
2025-06-14 21:49:39 +0200

    feat: add RepositoryContextFactory class

---
+++ CompanyEmployees/ContextFactory/RepositoryContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using CompanyEmployees.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace CompanyEmployees.ContextFactory
+{
+    public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
+    {
+        public RepositoryContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+           .SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("appsettings.json", optional: true)
+           .AddUserSecrets(typeof(Program).Assembly) // Ensure user secrets are loaded
+           .Build();
+
+            var connectionString = configuration.GetConnectionString("postgresConnection");
+
+            var builder = new DbContextOptionsBuilder<RepositoryContext>()
+                .UseNpgsql(connectionString,
+                    b => b.MigrationsAssembly("CompanyEmployees.Infrastructure.Persistence"));
+
+            return new RepositoryContext(builder.Options);
+        }
+    }
+}
