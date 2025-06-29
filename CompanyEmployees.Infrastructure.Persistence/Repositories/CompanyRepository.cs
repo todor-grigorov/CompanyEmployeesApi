fd3590119258e5e64b84f4e3a507f9a624c0599e
todor-grigorov <t.grigorov99@gmail.com>
2025-06-29 22:26:13 +0200

    feat: add CompanyRepository and EmployeeRepository

---
+++ CompanyEmployees.Infrastructure.Persistence/Repositories/CompanyRepository.cs
@@ -0,0 +1,13 @@
+﻿using CompanyEmployees.Core.Domain.Entities;
+using CompanyEmployees.Core.Domain.Repositories;
+
+namespace CompanyEmployees.Infrastructure.Persistence.Repositories
+{
+    public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
+    {
+        public CompanyRepository(RepositoryContext repositoryContext)
+            : base(repositoryContext)
+        {
+        }
+    }
+}
---
+++ CompanyEmployees.Infrastructure.Persistence/Repositories/EmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using CompanyEmployees.Core.Domain.Entities;
+using CompanyEmployees.Core.Domain.Repositories;
+
+namespace CompanyEmployees.Infrastructure.Persistence.Repositories
+{
+    public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
+    {
+        public EmployeeRepository(RepositoryContext repositoryContext)
+            : base(repositoryContext)
+        {
+        }
+    }
+}
