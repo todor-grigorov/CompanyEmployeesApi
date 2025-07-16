acd78a0037296dc588fa65d6dc09356bcf261ef0
todor-grigorov <t.grigorov99@gmail.com>
2025-07-16 22:47:17 +0200

    feat: add Repository Manager class and interface

---
+++ CompanyEmployees.Core.Domain/Repositories/IRepositoryManager.cs
@@ -0,0 +1,11 @@
+﻿using CompanyEmployees.Core.Domain.Repositories;
+
+namespace CompanyEmployees.Infrastructure.Persistence.Repositories
+{
+    public interface IRepositoryManager
+    {
+        ICompanyRepository Company { get; }
+        IEmployeeRepository Employee { get; }
+        void Save();
+    }
+}
