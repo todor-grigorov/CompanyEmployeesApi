448820e8ba43b0fc2766f7b4ea520d894edd20c4
todor-grigorov <t.grigorov99@gmail.com>
2025-08-26 15:31:40 +0200

    feat: implement EmployeeService

---
+++ CompanyEmployees.Core.Services/EmployeeService.cs
@@ -0,0 +1,17 @@
+﻿using CompanyEmployees.Core.Services.Abstractions;
+using CompanyEmployees.Infrastructure.Persistence.Repositories;
+using LoggingService;
+
+namespace CompanyEmployees.Core.Services
+{
+    internal sealed class EmployeeService : IEmployeeService 
+    {
+        private readonly IRepositoryManager _repository;
+        private readonly ILoggerManager _logger;
+        public EmployeeService(IRepositoryManager repository, ILoggerManager logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+    }
+}
