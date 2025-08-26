d7c93ccb79f385a2e58e13e85c8e7a1375dacb90
todor-grigorov <t.grigorov99@gmail.com>
2025-08-26 15:30:21 +0200

    feat: implement CompanyService

---
+++ CompanyEmployees.Core.Services/CompanyService.cs
@@ -0,0 +1,17 @@
+﻿using CompanyEmployees.Core.Services.Abstractions;
+using CompanyEmployees.Infrastructure.Persistence.Repositories;
+using LoggingService;
+
+namespace CompanyEmployees.Core.Services
+{
+    internal sealed class CompanyService : ICompanyService
+    {
+        private readonly IRepositoryManager _repository;
+        private readonly ILoggerManager _logger;
+        public CompanyService(IRepositoryManager repository, ILoggerManager logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+    }
+}
