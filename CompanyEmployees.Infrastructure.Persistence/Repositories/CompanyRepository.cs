def6de612f0e7e6a0360fbe4bebfd09238c50211
todor-grigorov <t.grigorov99@gmail.com>
2025-08-31 21:21:50 +0200

    feat: implement GetAllCompanies in CompanyRepository

--- CompanyEmployees.Infrastructure.Persistence/Repositories/CompanyRepository.cs
+++ CompanyEmployees.Infrastructure.Persistence/Repositories/CompanyRepository.cs
@@ -3,11 +3,16 @@ using CompanyEmployees.Core.Domain.Repositories;
 
 namespace CompanyEmployees.Infrastructure.Persistence.Repositories
 {
-    public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
+    internal sealed class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
     {
         public CompanyRepository(RepositoryContext repositoryContext)
             : base(repositoryContext)
         {
         }
+
+        public IEnumerable<Company> GetAllCompanies(bool trackChanges) =>
+        FindAll(trackChanges)
+            .OrderBy(c => c.Name)
+            .ToList();
     }
 }
