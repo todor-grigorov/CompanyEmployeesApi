ae54dcc08a590939fc6b30d061b71552aed81d0c
todor-grigorov <t.grigorov99@gmail.com>
2025-08-31 21:32:11 +0200

    feat: add CompaniesController with GetCompanies action method

---
+++ CompanyEmployees.Infrastructure.Presentation/Controllers/CompaniesController.cs
@@ -0,0 +1,27 @@
+﻿using CompanyEmployees.Core.Services.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyEmployees.Infrastructure.Presentation.Controllers
+{
+    [Route("api/companies")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
+    {
+        private readonly IServiceManager _service;
+        public CompaniesController(IServiceManager service) => _service = service;
+
+        [HttpGet]
+        public IActionResult GetCompanies()
+        {
+            try
+            {
+                var companies = _service.CompanyService.GetAllCompanies(trackChanges: false);
+                return Ok(companies);
+            }
+            catch
+            {
+                return StatusCode(500, "Internal server error");
+            }
+        }
+    }
+}
