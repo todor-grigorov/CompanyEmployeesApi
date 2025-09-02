c32fb6ed4b1322ea7c392c55c149dc566bfedc46
todor-grigorov <t.grigorov99@gmail.com>
2025-09-02 21:52:33 +0200

    feat: use CompanyDto in ComapnyService

--- CompanyEmployees.Core.Services/CompanyService.cs
+++ CompanyEmployees.Core.Services/CompanyService.cs
@@ -2,6 +2,7 @@
 using CompanyEmployees.Core.Services.Abstractions;
 using CompanyEmployees.Infrastructure.Persistence.Repositories;
 using LoggingService;
+using Shared.DataTransferObjects;
 
 namespace CompanyEmployees.Core.Services
 {
@@ -15,12 +16,17 @@ namespace CompanyEmployees.Core.Services
             _logger = logger;
         }
 
-        public IEnumerable<Company> GetAllCompanies(bool trackChanges)
+        public IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges)
         {
             try
             {
                 var companies = _repository.Company.GetAllCompanies(trackChanges);
-                return companies;
+
+                var companyDto = companies.Select(c => 
+                    new CompanyDto(c.Id, c.Name ?? "", string.Join(' ', c.Address, c.Country)))
+                    .ToList();
+
+                return companyDto;
             }
             catch (Exception ex)
             {
