388f344c4102787fdf735fd7484e1caaefe0c6e1
todor-grigorov <t.grigorov99@gmail.com>
2025-09-20 21:34:16 +0200

    feat: add Company/CompanyDto mapping profile

---
+++ CompanyEmployees/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CompanyEmployees.Core.Domain.Entities;
+using Shared.DataTransferObjects;
+
+namespace CompanyEmployees
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Company, CompanyDto>()
+                .ForCtorParam("FullAddress",
+                    opt => opt.MapFrom(x => $"{x.Address} {x.Country}"));
+        }
+    }
+}
