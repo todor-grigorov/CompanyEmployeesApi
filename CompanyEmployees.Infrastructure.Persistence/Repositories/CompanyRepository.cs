a989630cfaa0ec136d1c7d47bd9107b4f57261d1
todor-grigorov <t.grigorov99@gmail.com>
2025-09-22 23:47:53 +0200

    feat: implement GetCompany in CompanyRepository

--- CompanyEmployees.Infrastructure.Persistence/Repositories/CompanyRepository.cs
+++ CompanyEmployees.Infrastructure.Persistence/Repositories/CompanyRepository.cs
@@ -11,8 +11,12 @@ namespace CompanyEmployees.Infrastructure.Persistence.Repositories
         }
 
         public IEnumerable<Company> GetAllCompanies(bool trackChanges) =>
-        FindAll(trackChanges)
-            .OrderBy(c => c.Name)
-            .ToList();
+            FindAll(trackChanges)
+                .OrderBy(c => c.Name)
+                .ToList();
+
+        public Company GetCompany(Guid companyId, bool trackChanges) =>
+            FindByCondition(c => c.Id.Equals(companyId), trackChanges)
+                .SingleOrDefault()!;
     }
 }
