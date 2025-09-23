c0592c70a7b7a371eecda9ac01bf85e90cff1060
todor-grigorov <t.grigorov99@gmail.com>
2025-09-23 19:30:41 +0200

    feat: add GetCompany to ICompanyService

--- CompanyEmployees.Core.Services.Abstractions/ICompanyService.cs
+++ CompanyEmployees.Core.Services.Abstractions/ICompanyService.cs
@@ -6,5 +6,6 @@ namespace CompanyEmployees.Core.Services.Abstractions
     public interface ICompanyService
     {
         IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges);
+        CompanyDto GetCompany(Guid companyId, bool trackChanges);
     }
 }
