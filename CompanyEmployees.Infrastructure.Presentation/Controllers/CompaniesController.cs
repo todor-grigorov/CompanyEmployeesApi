758de6b4d75e8e88cffbb6e340510a9516299f38
todor-grigorov <t.grigorov99@gmail.com>
2025-09-23 19:33:37 +0200

    feat: add GetCompany by ID in CompaniesController

--- CompanyEmployees.Infrastructure.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees.Infrastructure.Presentation/Controllers/CompaniesController.cs
@@ -18,5 +18,13 @@ namespace CompanyEmployees.Infrastructure.Presentation.Controllers
             
             return Ok(companies);
         }
+
+        [HttpGet("{id:guid}")]
+        public IActionResult GetCompany(Guid id)
+        {
+            var company = _service.CompanyService.GetCompany(id, trackChanges: false);
+
+            return Ok(company);
+        }
     }
 }
