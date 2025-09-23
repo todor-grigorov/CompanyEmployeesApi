73382a7e48b1fb9052a28f840fad6d1a79e0837c
todor-grigorov <t.grigorov99@gmail.com>
2025-09-23 19:43:59 +0200

    feat: add CompanyNotFoundException

---
+++ CompanyEmployees.Core.Domain/Exceptions/CompanyNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace CompanyEmployees.Core.Domain.Exceptions
+{
+    public sealed class CompanyNotFoundException : NotFoundException
+    {
+        public CompanyNotFoundException(Guid companyId)
+            : base($"The company with id: {companyId} doesn't exist in the database.")
+        {
+        }
+    }
+}
