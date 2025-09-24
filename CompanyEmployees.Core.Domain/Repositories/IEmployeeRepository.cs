453c60e9869b9cfd37cfcf85ade64f0b3b946571
todor-grigorov <t.grigorov99@gmail.com>
2025-09-24 13:48:43 +0200

    feat: add GetEmployees to IEmployeeRepository

--- CompanyEmployees.Core.Domain/Repositories/IEmployeeRepository.cs
+++ CompanyEmployees.Core.Domain/Repositories/IEmployeeRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace CompanyEmployees.Core.Domain.Repositories
+﻿using CompanyEmployees.Core.Domain.Entities;
+
+namespace CompanyEmployees.Core.Domain.Repositories
 {
     public interface IEmployeeRepository
     {
+        IEnumerable<Employee> GetEmployees(Guid companyId, bool trackChanges);
     }
 }
