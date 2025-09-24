f8bca5ab356c3378b2cd9cc455d95a1283abc74b
todor-grigorov <t.grigorov99@gmail.com>
2025-09-24 14:18:54 +0200

    feat: add GetEmployees to IEmployeeService

--- CompanyEmployees.Core.Services.Abstractions/IEmployeeService.cs
+++ CompanyEmployees.Core.Services.Abstractions/IEmployeeService.cs
@@ -1,6 +1,9 @@
-﻿namespace CompanyEmployees.Core.Services.Abstractions
+﻿using Shared.DataTransferObjects;
+
+namespace CompanyEmployees.Core.Services.Abstractions
 {
     public interface IEmployeeService
     {
+        IEnumerable<EmployeeDto> GetEmployees(Guid companyId, bool trackChanges);
     }
 }
