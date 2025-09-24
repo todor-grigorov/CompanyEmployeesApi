d5a3a5d96e382d52c287cdce4443508418fbf10b
todor-grigorov <t.grigorov99@gmail.com>
2025-09-24 13:49:15 +0200

    feat: add EmployeesController

---
+++ CompanyEmployees.Infrastructure.Presentation/Controllers/EmployeesController.cs
@@ -0,0 +1,18 @@
+﻿using CompanyEmployees.Core.Services.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Infrastructure.Presentation.Controllers
+{
+    [Route("api/companies/{companyId}/employees")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IServiceManager _service;
+        public EmployeesController(IServiceManager service) => _service = service;
+    }
+}
