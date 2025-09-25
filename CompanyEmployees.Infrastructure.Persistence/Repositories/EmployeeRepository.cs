36e936fbf6425a504b26b2ffa17e7eb038315d87
todor-grigorov <t.grigorov99@gmail.com>
2025-09-25 21:09:00 +0200

    feat: implement GetEmployee in EmployeeRepository

--- CompanyEmployees.Infrastructure.Persistence/Repositories/EmployeeRepository.cs
+++ CompanyEmployees.Infrastructure.Persistence/Repositories/EmployeeRepository.cs
@@ -13,7 +13,11 @@ namespace CompanyEmployees.Infrastructure.Persistence.Repositories
 
         public IEnumerable<Employee> GetEmployees(Guid companyId, bool trackChanges) =>
             FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
-           .OrderBy(e => e.Name)
-           .ToList();
+            .OrderBy(e => e.Name)
+            .ToList();
+
+        public Employee GetEmployee(Guid companyId, Guid id, bool trackChanges) =>
+            FindByCondition(e => e.CompanyId.Equals(companyId) && e.Id.Equals(id), trackChanges)
+            .SingleOrDefault()!;
     }
 }
