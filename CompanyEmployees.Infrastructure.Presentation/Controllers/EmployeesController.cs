e95bcb769b259e3ba6ec833b7e374a5b5f04781f
todor-grigorov <t.grigorov99@gmail.com>
2025-09-26 20:34:56 +0200

    feat: add GetEmployeeForCompany to EmployeesController

--- CompanyEmployees.Infrastructure.Presentation/Controllers/EmployeesController.cs
+++ CompanyEmployees.Infrastructure.Presentation/Controllers/EmployeesController.cs
@@ -22,5 +22,13 @@ namespace CompanyEmployees.Infrastructure.Presentation.Controllers
 
             return Ok(employees);
         }
+
+        [HttpGet("{id:guid}")]
+        public IActionResult GetEmployeeForCompany(Guid companyId, Guid id)
+        {
+            var employee = _service.EmployeeService.GetEmployee(companyId, id, trackChanges: false);
+
+            return Ok(employee);
+        }
     }
 }
