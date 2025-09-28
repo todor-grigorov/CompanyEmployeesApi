0d213989b79e5ddd19901dd9fa5911447e1bea64
todor-grigorov <t.grigorov99@gmail.com>
2025-09-28 12:06:17 +0200

    feat: add CreateCompany to ICompanyService

--- CompanyEmployees.Core.Services.Abstractions/ICompanyService.cs
+++ CompanyEmployees.Core.Services.Abstractions/ICompanyService.cs
@@ -7,5 +7,6 @@ namespace CompanyEmployees.Core.Services.Abstractions
     {
         IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges);
         CompanyDto GetCompany(Guid companyId, bool trackChanges);
+        CompanyDto CreateCompany(CompanyForCreationDto company);
     }
 }
