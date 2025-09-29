4691bf198280d65b2664192a8668cad79ef91323
todor-grigorov <t.grigorov99@gmail.com>
2025-09-29 17:05:08 +0200

    feat: implement CreateEmployeeForCompany in EmployeeRepository

--- CompanyEmployees.Infrastructure.Persistence/Repositories/EmployeeRepository.cs
+++ CompanyEmployees.Infrastructure.Persistence/Repositories/EmployeeRepository.cs
@@ -19,5 +19,11 @@ namespace CompanyEmployees.Infrastructure.Persistence.Repositories
         public Employee GetEmployee(Guid companyId, Guid id, bool trackChanges) =>
             FindByCondition(e => e.CompanyId.Equals(companyId) && e.Id.Equals(id), trackChanges)
             .SingleOrDefault()!;
+
+        public void CreateEmployeeForCompany(Guid companyId, Employee employee)
+        {
+            employee.CompanyId = companyId;
+            Create(employee);
+        }
     }
 }
