6d81d2e93d9aee15a092bfd1623e68ed644317f0
todor-grigorov <t.grigorov99@gmail.com>
2025-09-30 14:47:58 +0200

    feat: add CreateEmployeeForCompany in EmployeesController

--- CompanyEmployees.Infrastructure.Presentation/Controllers/EmployeesController.cs
+++ CompanyEmployees.Infrastructure.Presentation/Controllers/EmployeesController.cs
@@ -1,5 +1,6 @@
 ﻿using CompanyEmployees.Core.Services.Abstractions;
 using Microsoft.AspNetCore.Mvc;
+using Shared.DataTransferObjects;
 
 namespace CompanyEmployees.Infrastructure.Presentation.Controllers
 {
@@ -24,6 +25,19 @@ namespace CompanyEmployees.Infrastructure.Presentation.Controllers
             var employee = _service.EmployeeService.GetEmployee(companyId, id, trackChanges: false);
 
             return Ok(employee);
+        }
+
+        [HttpPost]
+        public IActionResult CreateEmployeeForCompany(Guid companyId, [FromBody] EmployeeForCreationDto employee)
+        {
+            if (employee is null)
+                return BadRequest("EmployeeForCreationDto object is null");
+
+            var employeeToReturn = _service.EmployeeService.CreateEmployeeForCompany(companyId, employee,
+                trackChanges: false);
+
+            return CreatedAtRoute("GetEmployeeForCompany", new { companyId, id = employeeToReturn.Id },
+                employeeToReturn);
         }
     }
 }
