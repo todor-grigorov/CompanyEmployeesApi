462eff93cd25217dfffd848bc806a255c8885fda
todor-grigorov <t.grigorov99@gmail.com>
2025-10-01 14:08:55 +0200

    feat: add GetCompanyCollection to CompaniesController

--- CompanyEmployees.Infrastructure.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees.Infrastructure.Presentation/Controllers/CompaniesController.cs
@@ -19,6 +19,14 @@ namespace CompanyEmployees.Infrastructure.Presentation.Controllers
             return Ok(companies);
         }
 
+        [HttpGet("collection/({ids})", Name = "CompanyCollection")]
+        public IActionResult GetCompanyCollection(IEnumerable<Guid> ids)
+        {
+            var companies = _service.CompanyService.GetByIds(ids, trackChanges: false);
+
+            return Ok(companies);
+        }
+
         [HttpGet("{id:guid}", Name = "CompanyById")]
         public IActionResult GetCompany(Guid id)
         {
