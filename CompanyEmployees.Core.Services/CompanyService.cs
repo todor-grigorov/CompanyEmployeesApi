93d59a023f1816c6348e17c5daa677ec068d3740
todor-grigorov <t.grigorov99@gmail.com>
2025-10-01 14:05:48 +0200

    feat: add GetByIds in CompanyService

--- CompanyEmployees.Core.Services/CompanyService.cs
+++ CompanyEmployees.Core.Services/CompanyService.cs
@@ -53,5 +53,16 @@ namespace CompanyEmployees.Core.Services
 
             return companyToReturn;
         }
+
+        public IEnumerable<CompanyDto> GetByIds(IEnumerable<Guid> ids, bool trackChanges)
+        {
+            if (ids is null)
+                throw new IdParametersBadRequestException();
+            var companyEntities = _repository.Company.GetByIds(ids, trackChanges);
+            if (ids.Count() != companyEntities.Count())
+                throw new CollectionByIdsBadRequestException();
+            var companiesToReturn = _mapper.Map<IEnumerable<CompanyDto>>(companyEntities);
+            return companiesToReturn;
+        }
     }
 }
