18b4d2bdb054ef8316d8fb978cbe6dd6e4ba5df4
todor-grigorov <t.grigorov99@gmail.com>
2025-10-01 14:05:28 +0200

    feat: add IdParametersBadRequestException and CollectionByIdsBadRequestException

---
+++ CompanyEmployees.Core.Domain/Exceptions/IdParametersBadRequestException.cs
@@ -0,0 +1,18 @@
+﻿namespace CompanyEmployees.Core.Domain.Exceptions
+{
+    public sealed class IdParametersBadRequestException : BadRequestException
+    {
+        public IdParametersBadRequestException()
+            : base("Parameter ids is null")
+        {
+        }
+    }
+
+    public sealed class CollectionByIdsBadRequestException : BadRequestException
+    {
+        public CollectionByIdsBadRequestException()
+            : base("Collection count mismatch comparing to ids.")
+        {
+        }
+    }
+}
