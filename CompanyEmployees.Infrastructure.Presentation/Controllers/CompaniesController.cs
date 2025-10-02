c0ba4253c5cf885a371be5cb643ec4b252a024e9
todor-grigorov <t.grigorov99@gmail.com>
2025-10-02 17:00:40 +0200

    feat: update GetCompanyCollection to use ArrayModelBinder (CompaniesController)

--- CompanyEmployees.Infrastructure.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees.Infrastructure.Presentation/Controllers/CompaniesController.cs
@@ -1,4 +1,5 @@
 ﻿using CompanyEmployees.Core.Services.Abstractions;
+using CompanyEmployees.Infrastructure.Presentation.ModelBinders;
 using Microsoft.AspNetCore.Mvc;
 using Shared.DataTransferObjects;
 
@@ -20,7 +21,7 @@ namespace CompanyEmployees.Infrastructure.Presentation.Controllers
         }
 
         [HttpGet("collection/({ids})", Name = "CompanyCollection")]
-        public IActionResult GetCompanyCollection(IEnumerable<Guid> ids)
+        public IActionResult GetCompanyCollection([ModelBinder(BinderType = typeof(ArrayModelBinder))] IEnumerable<Guid> ids)
         {
             var companies = _service.CompanyService.GetByIds(ids, trackChanges: false);
 
