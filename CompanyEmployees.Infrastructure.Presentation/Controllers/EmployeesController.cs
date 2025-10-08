211e319278e365102acc48d5e3e4165f80512817
todor-grigorov <t.grigorov99@gmail.com>
2025-10-08 21:55:13 +0200

    feat: add UpdateEmployeeForCompanyto the EmployeesController

--- CompanyEmployees.Infrastructure.Presentation/Controllers/EmployeesController.cs
+++ CompanyEmployees.Infrastructure.Presentation/Controllers/EmployeesController.cs
@@ -47,5 +47,18 @@ namespace CompanyEmployees.Infrastructure.Presentation.Controllers
 
             return NoContent();
         }
+
+        [HttpPut("{id:guid}")]
+        public IActionResult UpdateEmployeeForCompany(Guid companyId, Guid id,
+                                                        [FromBody] EmployeeForUpdateDto employee)
+        {
+            if (employee is null)
+                return BadRequest("EmployeeForUpdateDto object is null");
+
+            _service.EmployeeService.UpdateEmployeeForCompany(companyId, id, employee,
+                compTrackChanges: false, empTrackChanges: true);
+
+            return NoContent();
+        }
     }
 }
