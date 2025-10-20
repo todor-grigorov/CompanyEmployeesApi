30f725cfc576c3314f899fe9d18b94ca2aa7e05f
todor-grigorov <t.grigorov99@gmail.com>
2025-10-20 20:57:04 +0200

    feat: use "init" setters for EmployeeForCreationDto

--- Shared/DataTransferObjects/EmployeeForCreationDto.cs
+++ Shared/DataTransferObjects/EmployeeForCreationDto.cs
@@ -2,13 +2,16 @@
 
 namespace Shared.DataTransferObjects;
 
-public record EmployeeForCreationDto(
+public record EmployeeForCreationDto
+{
     [Required(ErrorMessage = "Employee name is a required field.")]
     [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
-    string Name,
+    public string? Name { get; init; }
+
     [Required(ErrorMessage = "Age is a required field.")]
-    int Age,
+    public int Age { get; init; }
+
     [Required(ErrorMessage = "Position is a required field.")]
     [MaxLength(20, ErrorMessage = "Maximum length for the Position is 20 characters.")]
-    string Position
-);
+    public string? Position { get; init; }
+}
