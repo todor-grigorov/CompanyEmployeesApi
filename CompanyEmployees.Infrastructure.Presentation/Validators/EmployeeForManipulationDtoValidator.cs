db6b9e6fb6647e82b40d10c1b66156b0a6e19abb
todor-grigorov <t.grigorov99@gmail.com>
2025-10-23 23:24:56 +0200

    feat: add EmployeeForManipulationDtoValidator in  the Presentation project

---
+++ CompanyEmployees.Infrastructure.Presentation/Validators/EmployeeForManipulationDtoValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace Shared.DataTransferObjects
+{
+    public abstract class EmployeeForManipulationDtoValidator<T> : AbstractValidator<T>
+        where T : EmployeeForManipulationDto
+    {
+        public EmployeeForManipulationDtoValidator()
+        {
+            RuleFor(e => e.Name)
+                .NotEmpty().WithMessage("Employee name is a required field.")
+                .MaximumLength(30).WithMessage("\"Maximum length for the Name is 30 characters.");
+
+            RuleFor(e => e.Age)
+                .NotEmpty().WithMessage("Age is a required field.")
+                .GreaterThanOrEqualTo(18).WithMessage("Age can't be lower than 18.");
+
+            RuleFor(e => e.Position)
+                .NotEmpty().WithMessage("Position is a required field.")
+                .MaximumLength(20).WithMessage("Maximum length for the Position is 20 characters.");
+        }
+    }
+}
