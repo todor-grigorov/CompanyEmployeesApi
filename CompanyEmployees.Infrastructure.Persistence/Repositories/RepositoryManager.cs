1b789164cc96f513712bb9c3f75062f5718fbca5
todor-grigorov <t.grigorov99@gmail.com>
2025-10-25 00:21:25 +0200

    refactor: implement SaveAsync method in RepositoryManager

--- CompanyEmployees.Infrastructure.Persistence/Repositories/RepositoryManager.cs
+++ CompanyEmployees.Infrastructure.Persistence/Repositories/RepositoryManager.cs
@@ -15,6 +15,6 @@ namespace CompanyEmployees.Infrastructure.Persistence.Repositories
         }
         public ICompanyRepository Company => _companyRepository.Value;
         public IEmployeeRepository Employee => _employeeRepository.Value;
-        public void Save() => _repositoryContext.SaveChanges();
+        public async Task SaveAsync() => await _repositoryContext.SaveChangesAsync();
     }
 }
