0ab06da4e75942ce81afea05cc7c4cc90c4b040e
todor-grigorov <t.grigorov99@gmail.com>
2025-10-25 00:16:47 +0200

    feat: GetCompanyAsync to return null as well

--- CompanyEmployees.Core.Domain/Repositories/ICompanyRepository.cs
+++ CompanyEmployees.Core.Domain/Repositories/ICompanyRepository.cs
@@ -5,7 +5,7 @@ namespace CompanyEmployees.Core.Domain.Repositories
     public interface ICompanyRepository
     {
         Task<IEnumerable<Company>> GetAllCompaniesAsync(bool trackChanges);
-        Task<Company> GetCompanyAsync(Guid companyId, bool trackChanges);
+        Task<Company?> GetCompanyAsync(Guid companyId, bool trackChanges);
         void CreateCompany(Company company);
         Task<IEnumerable<Company>> GetByIdsAsync(IEnumerable<Guid> ids, bool trackChanges);
         void DeleteCompany(Company company);
