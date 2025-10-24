2b58ed2626cbaeb65c85c9e82af8e5ec610ba4d2
todor-grigorov <t.grigorov99@gmail.com>
2025-10-24 15:20:29 +0200

    feat: make some of the methods in the ICompanyRepository asyncronous

--- CompanyEmployees.Core.Domain/Repositories/ICompanyRepository.cs
+++ CompanyEmployees.Core.Domain/Repositories/ICompanyRepository.cs
@@ -4,14 +4,10 @@ namespace CompanyEmployees.Core.Domain.Repositories
 {
     public interface ICompanyRepository
     {
-        IEnumerable<Company> GetAllCompanies(bool trackChanges);
-
-        Company GetCompany(Guid companyId, bool trackChanges);
-
+        Task<IEnumerable<Company>> GetAllCompaniesAsync(bool trackChanges);
+        Task<Company> GetCompanyAsync(Guid companyId, bool trackChanges);
         void CreateCompany(Company company);
-
-        IEnumerable<Company> GetByIds(IEnumerable<Guid> ids, bool trackChanges);
-
+        Task<IEnumerable<Company>> GetByIdsAsync(IEnumerable<Guid> ids, bool trackChanges);
         void DeleteCompany(Company company);
     }
 }
