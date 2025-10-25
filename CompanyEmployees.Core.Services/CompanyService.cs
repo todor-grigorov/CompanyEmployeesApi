397a502b268e4b41b0e03f7321456564c0d17a78
todor-grigorov <t.grigorov99@gmail.com>
2025-10-25 23:42:41 +0200

    feat: implement DeleteCompanyAsync (CompanyService)

--- CompanyEmployees.Core.Services/CompanyService.cs
+++ CompanyEmployees.Core.Services/CompanyService.cs
@@ -88,14 +88,14 @@ namespace CompanyEmployees.Core.Services
             return (companies: companyCollectionToReturn, ids: ids);
         }
 
-        public void DeleteCompany(Guid companyId, bool trackChanges)
+        public async Task DeleteCompanyAsync(Guid companyId, bool trackChanges)
         {
-            var company = _repository.Company.GetCompany(companyId, trackChanges);
+            var company = await _repository.Company.GetCompanyAsync(companyId, trackChanges);
             if (company is null)
                 throw new CompanyNotFoundException(companyId);
 
             _repository.Company.DeleteCompany(company);
-            _repository.Save();
+            await _repository.SaveAsync();
         }
 
         public void UpdateCompany(Guid companyId, CompanyForUpdateDto companyForUpdate, bool trackChanges)
