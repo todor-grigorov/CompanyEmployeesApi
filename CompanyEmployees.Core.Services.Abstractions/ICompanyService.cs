3f0bbb663f8017eaa450bf3a73dccc638176306c
todor-grigorov <t.grigorov99@gmail.com>
2025-10-25 23:33:33 +0200

    refactor: make all services in ICompanyService asynchromous

--- CompanyEmployees.Core.Services.Abstractions/ICompanyService.cs
+++ CompanyEmployees.Core.Services.Abstractions/ICompanyService.cs
@@ -5,18 +5,20 @@ namespace CompanyEmployees.Core.Services.Abstractions
 {
     public interface ICompanyService
     {
-        IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges);
+        Task<IEnumerable<CompanyDto>> GetAllCompaniesAsync(bool trackChanges);
 
-        CompanyDto GetCompany(Guid companyId, bool trackChanges);
+        Task<CompanyDto> GetCompanyAsync(Guid companyId, bool trackChanges);
 
-        CompanyDto CreateCompany(CompanyForCreationDto company);
+        Task<CompanyDto> CreateCompanyAsync(CompanyForCreationDto company);
 
-        IEnumerable<CompanyDto> GetByIds(IEnumerable<Guid> ids, bool trackChanges);
+        Task<IEnumerable<CompanyDto>> GetByIdsAsync(IEnumerable<Guid> ids, bool trackChanges);
 
-        (IEnumerable<CompanyDto> companies, string ids) CreateCompanyCollection(IEnumerable<CompanyForCreationDto> companyCollection);
+        Task<(IEnumerable<CompanyDto> companies, string ids)> CreateCompanyCollectionAsync
+            (IEnumerable<CompanyForCreationDto> companyCollection);
 
-        void DeleteCompany(Guid companyId, bool trackChanges);
+        Task DeleteCompanyAsync(Guid companyId, bool trackChanges);
 
-        void UpdateCompany(Guid companyid, CompanyForUpdateDto companyForUpdate, bool trackChanges);
+        Task UpdateCompanyAsync(Guid companyid, CompanyForUpdateDto companyForUpdate,
+            bool trackChanges);
     }
 }
