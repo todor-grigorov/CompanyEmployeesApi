200c1fc50a0e312d9f7743c086350311d37150e2
todor-grigorov <t.grigorov99@gmail.com>
2025-10-27 11:41:21 +0100

    refactor: all methos in IEmployeeService to be Async

--- CompanyEmployees.Core.Services.Abstractions/IEmployeeService.cs
+++ CompanyEmployees.Core.Services.Abstractions/IEmployeeService.cs
@@ -5,22 +5,22 @@ namespace CompanyEmployees.Core.Services.Abstractions
 {
     public interface IEmployeeService
     {
-        IEnumerable<EmployeeDto> GetEmployees(Guid companyId, bool trackChanges);
+        Task<IEnumerable<EmployeeDto>> GetEmployeesAsync(Guid companyId, bool trackChanges);
 
-        EmployeeDto GetEmployee(Guid companyId, Guid id, bool trackChanges);
+        Task<EmployeeDto> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges);
 
-        EmployeeDto CreateEmployeeForCompany(Guid companyId, EmployeeForCreationDto employeeForCreation,
+        Task<EmployeeDto> CreateEmployeeForCompanyAsync(Guid companyId, EmployeeForCreationDto employeeForCreation,
                                                 bool trackChanges);
 
-        void DeleteEmployeeForCompany(Guid companyId, Guid id, bool trackChanges);
+        Task DeleteEmployeeForCompanyAsync(Guid companyId, Guid id, bool trackChanges);
 
-        void UpdateEmployeeForCompany(Guid companyId, Guid id,
+        Task UpdateEmployeeForCompanyAsync(Guid companyId, Guid id,
                                         EmployeeForUpdateDto employeeForUpdate, bool compTrackChanges, bool empTrackChanges);
 
-        (EmployeeForUpdateDto employeeToPatch, Employee employeeEntity) GetEmployeeForPatch(
+        Task<(EmployeeForUpdateDto employeeToPatch, Employee employeeEntity)> GetEmployeeForPatchAsync(
                                         Guid companyId, Guid id, bool compTrackChanges, bool empTrackChanges);
 
-        void SaveChangesForPatch(EmployeeForUpdateDto employeeToPatch, Employee employeeEntity);
+        Task SaveChangesForPatchAsync(EmployeeForUpdateDto employeeToPatch, Employee employeeEntity);
 
     }
 }
