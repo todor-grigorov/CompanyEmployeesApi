28d7d6c4588fa279d97844c2c0c6ad0ae75c97fc
todor-grigorov <t.grigorov99@gmail.com>
2025-10-29 22:52:33 +0100

    feat: implement the usage of CancellationToken in SaveAsync (RepositoryManager)

--- CompanyEmployees.Infrastructure.Persistence/Repositories/RepositoryManager.cs
+++ CompanyEmployees.Infrastructure.Persistence/Repositories/RepositoryManager.cs
@@ -15,6 +15,6 @@ namespace CompanyEmployees.Infrastructure.Persistence.Repositories
         }
         public ICompanyRepository Company => _companyRepository.Value;
         public IEmployeeRepository Employee => _employeeRepository.Value;
-        public async Task SaveAsync() => await _repositoryContext.SaveChangesAsync();
+        public async Task SaveAsync(CancellationToken ct = default) => await _repositoryContext.SaveChangesAsync(ct);
     }
 }
