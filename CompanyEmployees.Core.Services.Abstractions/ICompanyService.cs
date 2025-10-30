788f3e9aac0ab29d5c745a0a375551cb0a647aef
todor-grigorov <t.grigorov99@gmail.com>
2025-10-30 20:29:00 +0100

    feat: add CancellationTokenin some methods in ICompanyService

--- CompanyEmployees.Core.Services.Abstractions/ICompanyService.cs
+++ CompanyEmployees.Core.Services.Abstractions/ICompanyService.cs
@@ -5,11 +5,11 @@ namespace CompanyEmployees.Core.Services.Abstractions
 {
     public interface ICompanyService
     {
-        Task<IEnumerable<CompanyDto>> GetAllCompaniesAsync(bool trackChanges);
+        Task<IEnumerable<CompanyDto>> GetAllCompaniesAsync(bool trackChanges, CancellationToken ct = default);
 
         Task<CompanyDto> GetCompanyAsync(Guid companyId, bool trackChanges);
 
-        Task<CompanyDto> CreateCompanyAsync(CompanyForCreationDto company);
+        Task<CompanyDto> CreateCompanyAsync(CompanyForCreationDto company, CancellationToken ct = default);
 
         Task<IEnumerable<CompanyDto>> GetByIdsAsync(IEnumerable<Guid> ids, bool trackChanges);
 
