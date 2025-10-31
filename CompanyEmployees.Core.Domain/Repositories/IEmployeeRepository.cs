0b82c297bc655850f071f90f00b0d42af562f5c9
todor-grigorov <t.grigorov99@gmail.com>
2025-10-31 22:24:42 +0100

    feat: add Cancellation Tokens to methods in IEmployeeRepository

--- CompanyEmployees.Core.Domain/Repositories/IEmployeeRepository.cs
+++ CompanyEmployees.Core.Domain/Repositories/IEmployeeRepository.cs
@@ -4,12 +4,12 @@ namespace CompanyEmployees.Core.Domain.Repositories
 {
     public interface IEmployeeRepository
     {
-        Task<IEnumerable<Employee>> GetEmployeesAsync(Guid companyId, bool trackChanges);
+        Task<IEnumerable<Employee>> GetEmployeesAsync(Guid companyId, bool trackChanges, CancellationToken ct = default);
 
-        Task<Employee?> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges);
+        Task<Employee?> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges, CancellationToken ct = default);
 
         void CreateEmployeeForCompany(Guid companyId, Employee employee);
 
-        Task DeleteEmployeeAsync(Company company, Employee employee);
+        Task DeleteEmployeeAsync(Company company, Employee employee, CancellationToken ct = default);
     }
 }
