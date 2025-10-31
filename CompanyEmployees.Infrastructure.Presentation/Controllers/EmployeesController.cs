177f180af535ab4d24a4b98b35c0e68b86464db3
todor-grigorov <t.grigorov99@gmail.com>
2025-10-31 22:25:35 +0100

    feat: add Cancellation Tokens to actions in EmployeesController

--- CompanyEmployees.Infrastructure.Presentation/Controllers/EmployeesController.cs
+++ CompanyEmployees.Infrastructure.Presentation/Controllers/EmployeesController.cs
@@ -3,7 +3,7 @@ using FluentValidation;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Shared.DataTransferObjects;
-using System.ComponentModel.DataAnnotations;
+//using System.ComponentModel.DataAnnotations;
 
 namespace CompanyEmployees.Infrastructure.Presentation.Controllers
 {
@@ -15,23 +15,24 @@ namespace CompanyEmployees.Infrastructure.Presentation.Controllers
         public EmployeesController(IServiceManager service) => _service = service;
 
         [HttpGet]
-        public async Task<IActionResult> GetEmployeesForCompany(Guid companyId)
+        public async Task<IActionResult> GetEmployeesForCompany(Guid companyId, CancellationToken ct)
         {
-            var employees = await _service.EmployeeService.GetEmployeesAsync(companyId, trackChanges: false);
+            var employees = await _service.EmployeeService.GetEmployeesAsync(companyId, trackChanges: false, ct);
 
             return Ok(employees);
         }
 
         [HttpGet("{id:guid}", Name = "GetEmployeeForCompany")]
-        public async Task<IActionResult> GetEmployeeForCompany(Guid companyId, Guid id)
+        public async Task<IActionResult> GetEmployeeForCompany(Guid companyId, Guid id, CancellationToken ct)
         {
-            var employee = await _service.EmployeeService.GetEmployeeAsync(companyId, id, trackChanges: false);
+            var employee = await _service.EmployeeService.GetEmployeeAsync(companyId, id, trackChanges: false, ct);
 
             return Ok(employee);
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateEmployeeForCompany(Guid companyId, [FromBody] EmployeeForCreationDto employee, [FromServices] IValidator<EmployeeForCreationDto> validator)
+        public async Task<IActionResult> CreateEmployeeForCompany(Guid companyId, [FromBody] EmployeeForCreationDto employee, 
+            [FromServices] IValidator<EmployeeForCreationDto> validator, CancellationToken ct)
         {
             if (employee is null)
                 return BadRequest("EmployeeForCreationDto object is null");
@@ -44,16 +45,16 @@ namespace CompanyEmployees.Infrastructure.Presentation.Controllers
             //    return UnprocessableEntity(ModelState);
 
             var employeeToReturn = await _service.EmployeeService.CreateEmployeeForCompanyAsync(companyId, employee,
-                trackChanges: false);
+                trackChanges: false, ct);
 
             return CreatedAtRoute("GetEmployeeForCompany", new { companyId, id = employeeToReturn.Id },
                 employeeToReturn);
         }
 
         [HttpDelete("{id:guid}")]
-        public async Task<IActionResult> DeleteEmployeeForCompany(Guid companyId, Guid id)
+        public async Task<IActionResult> DeleteEmployeeForCompany(Guid companyId, Guid id, CancellationToken ct)
         {
-            await _service.EmployeeService.DeleteEmployeeForCompanyAsync(companyId, id, trackChanges: false);
+            await _service.EmployeeService.DeleteEmployeeForCompanyAsync(companyId, id, trackChanges: false, ct);
 
             return NoContent();
         }
@@ -61,7 +62,8 @@ namespace CompanyEmployees.Infrastructure.Presentation.Controllers
         [HttpPut("{id:guid}")]
         public async Task<IActionResult> UpdateEmployeeForCompany(Guid companyId, Guid id,
                                                         [FromBody] EmployeeForUpdateDto employee,
-                                                        [FromServices] IValidator<EmployeeForUpdateDto> validator)
+                                                        [FromServices] IValidator<EmployeeForUpdateDto> validator, 
+                                                        CancellationToken ct)
         {
             if (employee is null)
                 return BadRequest("EmployeeForUpdateDto object is null");
@@ -74,20 +76,21 @@ namespace CompanyEmployees.Infrastructure.Presentation.Controllers
             //    return UnprocessableEntity(ModelState);
 
             await _service.EmployeeService.UpdateEmployeeForCompanyAsync(companyId, id, employee,
-                compTrackChanges: false, empTrackChanges: true);
+                compTrackChanges: false, empTrackChanges: true, ct);
 
             return NoContent();
         }
 
         [HttpPatch("{id:guid}")]
         public async Task<IActionResult> PartiallyUpdateEmployeeForCompany(Guid companyId, Guid id,
-                                                                [FromBody] JsonPatchDocument<EmployeeForUpdateDto> patchDoc)
+                                                                [FromBody] JsonPatchDocument<EmployeeForUpdateDto> patchDoc, 
+                                                                CancellationToken ct)
         {
             if (patchDoc is null)
                 return BadRequest("patchDoc object sent from client is null.");
 
             var result = await _service.EmployeeService.GetEmployeeForPatchAsync(companyId, id, compTrackChanges: false,
-                empTrackChanges: true);
+                empTrackChanges: true, ct);
 
             patchDoc.ApplyTo(result.employeeToPatch, ModelState);
 
@@ -96,7 +99,7 @@ namespace CompanyEmployees.Infrastructure.Presentation.Controllers
             if (!ModelState.IsValid)
                 return UnprocessableEntity(ModelState);
 
-            await _service.EmployeeService.SaveChangesForPatchAsync(result.employeeToPatch, result.employeeEntity);
+            await _service.EmployeeService.SaveChangesForPatchAsync(result.employeeToPatch, result.employeeEntity, ct);
 
             return NoContent();
         }
