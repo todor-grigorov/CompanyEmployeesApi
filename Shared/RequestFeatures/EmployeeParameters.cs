9c6ac0722f8d118d087744253860e623da8f1b03
todor-grigorov <t.grigorov99@gmail.com>
2025-12-28 20:27:14 +0100

    feat: add SearchTerm prop in EmployeeParameters class

--- Shared/RequestFeatures/EmployeeParameters.cs
+++ Shared/RequestFeatures/EmployeeParameters.cs
@@ -4,6 +4,9 @@
     {
         public uint MinAge { get; set; }
         public uint MaxAge { get; set; } = int.MaxValue;
+
         public bool ValidAgeRange => MaxAge > MinAge;
+
+        public string? SearchTerm { get; set; }
     }
 }
