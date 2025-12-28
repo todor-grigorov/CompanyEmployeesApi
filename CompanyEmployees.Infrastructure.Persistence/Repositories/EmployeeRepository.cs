1ee3c441714da71ed353ff6fcc1504c349325bcc
todor-grigorov <t.grigorov99@gmail.com>
2025-12-28 19:51:17 +0100

    feat: allow X-Pagination header in the CORS policy

--- CompanyEmployees.Infrastructure.Persistence/Repositories/EmployeeRepository.cs
+++ CompanyEmployees.Infrastructure.Persistence/Repositories/EmployeeRepository.cs
@@ -17,9 +17,15 @@ namespace CompanyEmployees.Infrastructure.Persistence.Repositories
         {
             var employees = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
             .OrderBy(e => e.Name)
+            .Skip((employeeParameters.PageNumber - 1) * employeeParameters.PageSize)
+            .Take(employeeParameters.PageSize)
             .ToListAsync(ct);
 
-            return PagedList<Employee>.ToPagedList(employees, employeeParameters.PageNumber, employeeParameters.PageSize);
+            var count = await FindByCondition(e => e.CompanyId.Equals(companyId),
+                trackChanges)
+                .CountAsync(ct);
+
+            return PagedList<Employee>.ToPagedList(employees, count, employeeParameters.PageNumber, employeeParameters.PageSize);
         }
         public async Task<Employee?> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges, CancellationToken ct = default) =>
             await FindByCondition(e => e.CompanyId.Equals(companyId) && e.Id.Equals(id), trackChanges)
