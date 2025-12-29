8fdd45d93fe59e9c63117c358c7f6120baeb887e
todor-grigorov <t.grigorov99@gmail.com>
2025-12-29 22:59:41 +0100

    feat: add IDataShaper interface in the Abstractions project

---
+++ CompanyEmployees.Core.Services.Abstractions/IDataShaper.cs
@@ -0,0 +1,10 @@
+﻿using System.Dynamic;
+
+namespace CompanyEmployees.Core.Services.Abstractions
+{
+    public interface IDataShaper<T>
+    {
+        IEnumerable<ExpandoObject> ShapeData(IEnumerable<T> entities, string fieldsString);
+        ExpandoObject ShapeData(T entity, string fieldsString);
+    }
+}
