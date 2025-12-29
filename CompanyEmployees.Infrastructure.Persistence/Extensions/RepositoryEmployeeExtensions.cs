9a9bdb440ebe0dc295a80ee1ca492c4a72476ccd
todor-grigorov <t.grigorov99@gmail.com>
2025-12-29 20:58:21 +0100

    feat: create OrderQueryBuilder class ant extract logic in CreateOrderQuery method, then use the method inside the Sort/RepositoryEmployeeExtensions

--- CompanyEmployees.Infrastructure.Persistence/Extensions/RepositoryEmployeeExtensions.cs
+++ CompanyEmployees.Infrastructure.Persistence/Extensions/RepositoryEmployeeExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using CompanyEmployees.Core.Domain.Entities;
+using CompanyEmployees.Infrastructure.Persistence.Extensions.Utility;
 using System.Linq.Dynamic.Core;
-using System.Reflection;
-using System.Text;
 
 namespace CompanyEmployees.Infrastructure.Persistence.Extensions
 {
@@ -23,28 +22,7 @@ namespace CompanyEmployees.Infrastructure.Persistence.Extensions
             if (string.IsNullOrWhiteSpace(orderByQueryString))
                 return employees.OrderBy(e => e.Name);
 
-            var orderParams = orderByQueryString.Trim().Split(',');
-            var propertyInfos = typeof(Employee).GetProperties(BindingFlags.Public | BindingFlags.Instance);
-            var orderQueryBuilder = new StringBuilder();
-
-            foreach (var param in orderParams)
-            {
-                if (string.IsNullOrWhiteSpace(param))
-                    continue;
-
-                var propertyFromQueryName = param.Split(" ")[0];
-                var objectProperty = propertyInfos.FirstOrDefault(pi =>
-                    pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
-
-                if (objectProperty is null)
-                    continue;
-
-                var direction = param.EndsWith(" desc") ? "descending" : "ascending";
-
-                orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction}, ");
-            }
-
-            var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+            var orderQuery = OrderQueryBuilder.CreateOrderQuery<Employee>(orderByQueryString);
 
             if (string.IsNullOrWhiteSpace(orderQuery))
                 return employees.OrderBy(e => e.Name);
