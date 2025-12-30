29e44dfc24ca2cd7ff1c0ed8ea1261cc8c140d97
todor-grigorov <t.grigorov99@gmail.com>
2025-12-30 21:15:28 +0100

    feat: add LinkResourceBase class in the Domain project

---
+++ CompanyEmployees.Core.Domain/LinkModels/LinkResourceBase.cs
@@ -0,0 +1,9 @@
+﻿namespace CompanyEmployees.Core.Domain.LinkModels
+{
+    public class LinkResourceBase
+    {
+        public LinkResourceBase()
+        { }
+        public List<Link> Links { get; set; } = new List<Link>();
+    }
+}
