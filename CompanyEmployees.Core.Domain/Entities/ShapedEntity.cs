4387b7df3b01375b34fc4bcbe6e1585e8bde24e7
todor-grigorov <t.grigorov99@gmail.com>
2025-12-30 21:31:00 +0100

    feat: add ShapedEntity class in Domain project

---
+++ CompanyEmployees.Core.Domain/Entities/ShapedEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace CompanyEmployees.Core.Domain.Entities
+{
+    public class ShapedEntity
+    {
+        public ShapedEntity()
+        {
+            Entity = new Entity();
+        }
+        public Guid Id { get; set; }
+        public Entity Entity { get; set; }
+    }
+}
