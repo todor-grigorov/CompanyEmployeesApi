17d152be0ec0c7c4f0a39745b7866402e6db6262
todor-grigorov <t.grigorov99@gmail.com>
2025-12-30 21:39:47 +0100

    feat: add LinkResponse class in Domain project

---
+++ CompanyEmployees.Core.Domain/LinkModels/LinkResponse.cs
@@ -0,0 +1,16 @@
+﻿using CompanyEmployees.Core.Domain.Entities;
+
+namespace CompanyEmployees.Core.Domain.LinkModels
+{
+    public class LinkResponse
+    {
+        public bool HasLinks { get; set; }
+        public List<Entity> ShapedEntities { get; set; }
+        public LinkCollectionWrapper<Entity> LinkedEntities { get; set; }
+        public LinkResponse()
+        {
+            LinkedEntities = new LinkCollectionWrapper<Entity>();
+            ShapedEntities = new List<Entity>();
+        }
+    }
+}
