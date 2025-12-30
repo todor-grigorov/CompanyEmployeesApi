b7c3065fcded72b5f18c17468a92330106a91143
todor-grigorov <t.grigorov99@gmail.com>
2025-12-30 21:12:43 +0100

    refactor: inject IDataShaper in the ServiceManager and EmployeeService classes

--- CompanyEmployees.Core.Services/ServiceManager.cs
+++ CompanyEmployees.Core.Services/ServiceManager.cs
@@ -2,6 +2,7 @@
 using CompanyEmployees.Core.Services.Abstractions;
 using CompanyEmployees.Infrastructure.Persistence.Repositories;
 using LoggingService;
+using Shared.DataTransferObjects;
 
 namespace CompanyEmployees.Core.Services
 {
@@ -9,10 +10,10 @@ namespace CompanyEmployees.Core.Services
     {
         private readonly Lazy<ICompanyService> _companyService;
         private readonly Lazy<IEmployeeService> _employeeService;
-        public ServiceManager(IRepositoryManager repositoryManager, ILoggerManager logger, IMapper mapper)
+        public ServiceManager(IRepositoryManager repositoryManager, ILoggerManager logger, IMapper mapper, IDataShaper<EmployeeDto> dataShaper)
         {
             _companyService = new Lazy<ICompanyService>(() => new CompanyService(repositoryManager, logger, mapper));
-            _employeeService = new Lazy<IEmployeeService>(() => new EmployeeService(repositoryManager, logger, mapper));
+            _employeeService = new Lazy<IEmployeeService>(() => new EmployeeService(repositoryManager, logger, mapper, dataShaper));
         }
         public ICompanyService CompanyService => _companyService.Value;
         public IEmployeeService EmployeeService => _employeeService.Value;
