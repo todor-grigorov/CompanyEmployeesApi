035f21618246d9af8d9f94a291e61d20c99f07c2
todor-grigorov <t.grigorov99@gmail.com>
2025-12-31 13:07:23 +0100

    feat: add IEmployeeLinks to the Abstractions project

---
+++ CompanyEmployees.Core.Services.Abstractions/IEmployeeLinks.cs
@@ -0,0 +1,12 @@
+﻿using CompanyEmployees.Core.Domain.LinkModels;
+using Microsoft.AspNetCore.Http;
+using Shared.DataTransferObjects;
+
+namespace CompanyEmployees.Core.Services.Abstractions
+{
+    public interface IEmployeeLinks
+    {
+        LinkResponse TryGenerateLinks(IEnumerable<EmployeeDto> employeesDto,
+        string fields, Guid companyId, HttpContext httpContext);
+    }
+}
