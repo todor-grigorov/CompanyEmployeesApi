eaae18e95448d7c2b6c43038d4d252740b1c2ec9
todor-grigorov <t.grigorov99@gmail.com>
2025-12-31 14:19:46 +0100

    feat: add LinkParameters record in the Domain project

---
+++ CompanyEmployees.Core.Domain/LinkModels/LinkParameters.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Http;
+using Shared.RequestFeatures;
+
+namespace CompanyEmployees.Core.Domain.LinkModels
+{
+    public record LinkParameters(EmployeeParameters EmployeeParameters, HttpContext Context);
+}
