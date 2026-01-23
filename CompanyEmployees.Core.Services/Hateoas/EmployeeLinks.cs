1036979219a5e6e3e85c90099371464be0273f04
todor-grigorov <t.grigorov99@gmail.com>
2026-01-23 22:48:00 +0100

    chore: code formatting

--- CompanyEmployees.Core.Services/Hateoas/EmployeeLinks.cs
+++ CompanyEmployees.Core.Services/Hateoas/EmployeeLinks.cs
@@ -14,6 +14,7 @@ namespace CompanyEmployees.Core.Services.Hateoas
         private readonly IDataShaper<EmployeeDto> _dataShaper;
         public Dictionary<string, MediaTypeHeaderValue> AcceptHeader { get; set; } =
             new Dictionary<string, MediaTypeHeaderValue>();
+
         public EmployeeLinks(LinkGenerator linkGenerator, IDataShaper<EmployeeDto> dataShaper)
         {
             _linkGenerator = linkGenerator;
@@ -55,28 +56,28 @@ namespace CompanyEmployees.Core.Services.Hateoas
             var linkedEmployees = CreateLinksForEmployees(httpContext, employeeCollection);
             return new LinkResponse { HasLinks = true, LinkedEntities = linkedEmployees };
         }
-        private List<Link> CreateLinksForEmployee(HttpContext httpContext, Guid companyId, Guid id,
-    string fields = "")
+
+        private List<Link> CreateLinksForEmployee(HttpContext httpContext, Guid companyId, Guid id, string fields = "")
         {
             var links = new List<Link>
-    {
-        new Link(_linkGenerator.GetUriByAction(httpContext, "GetEmployeeForCompany",
-            values: new { companyId, id, fields })!,
-        "self",
-        "GET"),
-        new Link(_linkGenerator.GetUriByAction(httpContext, "DeleteEmployeeForCompany",
-            values: new { companyId, id })!,
-        "delete_employee",
-        "DELETE"),
-        new Link(_linkGenerator.GetUriByAction(httpContext, "UpdateEmployeeForCompany",
-            values: new { companyId, id })!,
-        "update_employee",
-        "PUT"),
-        new Link(_linkGenerator.GetUriByAction(httpContext, "PartiallyUpdateEmployeeForCompany",
-            values: new { companyId, id })!,
-        "partially_update_employee",
-        "PATCH")
-    };
+            {
+                new Link(_linkGenerator.GetUriByAction(httpContext, "GetEmployeeForCompany",
+                    values: new { companyId, id, fields })!,
+                "self",
+                "GET"),
+                new Link(_linkGenerator.GetUriByAction(httpContext, "DeleteEmployeeForCompany",
+                    values: new { companyId, id })!,
+                "delete_employee",
+                "DELETE"),
+                new Link(_linkGenerator.GetUriByAction(httpContext, "UpdateEmployeeForCompany",
+                    values: new { companyId, id })!,
+                "update_employee",
+                "PUT"),
+                new Link(_linkGenerator.GetUriByAction(httpContext, "PartiallyUpdateEmployeeForCompany",
+                    values: new { companyId, id })!,
+                "partially_update_employee",
+                "PATCH")
+            };
 
             return links;
         }
