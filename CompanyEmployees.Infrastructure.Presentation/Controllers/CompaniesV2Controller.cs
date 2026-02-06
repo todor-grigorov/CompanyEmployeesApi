b053301d6edbf3948411d40e47511c352473a69d
todor-grigorov <t.grigorov99@gmail.com>
2026-02-06 15:51:10 +0100

    feat: add Compnies controller for Version 2

---
+++ CompanyEmployees.Infrastructure.Presentation/Controllers/CompaniesV2Controller.cs
@@ -0,0 +1,23 @@
+﻿using CompanyEmployees.Core.Services.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyEmployees.Infrastructure.Presentation.Controllers
+{
+    [Route("api/{v:apiversion}/companies")]
+    [ApiController]
+    public class CompaniesV2Controller : ControllerBase
+    {
+        private readonly IServiceManager _service;
+        public CompaniesV2Controller(IServiceManager service) => _service = service;
+        [HttpGet]
+        public async Task<IActionResult> GetCompanies()
+        {
+            var companies = await _service.CompanyService
+                .GetAllCompaniesAsync(trackChanges: false);
+
+            var companiesV2 = companies.Select(x => $"{x.Name} V2");
+
+            return Ok(companiesV2);
+        }
+    }
+}
