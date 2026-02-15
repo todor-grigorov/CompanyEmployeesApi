ba6107efd2f83b061f94ccafdea2a8383b7f5dd0
todor-grigorov <t.grigorov99@gmail.com>
2026-02-15 22:40:42 +0100

    feat: add DB migration for Identity tables

--- CompanyEmployees.Infrastructure.Persistence/Migrations/20260215213929_CreatingIdentityTables.Designer.cs
+++ CompanyEmployees.Infrastructure.Persistence/Migrations/20260215213929_CreatingIdentityTables.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CompanyEmployees.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CompanyEmployees.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(RepositoryContext))]
-    partial class RepositoryContextModelSnapshot : ModelSnapshot
+    [Migration("20260215213929_CreatingIdentityTables")]
+    partial class CreatingIdentityTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
