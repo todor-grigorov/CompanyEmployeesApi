1ef526a857c8f3210ffc4efba869e452bf70b378
todor-grigorov <t.grigorov99@gmail.com>
2026-02-15 22:46:59 +0100

    feat: add AddedRolesToDb db migration

---
+++ CompanyEmployees.Infrastructure.Persistence/Migrations/20260215214344_AddedRolesToDb.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CompanyEmployees.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedRolesToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "464e58b6-5934-4314-a04a-af4033d8a801", null, "Manager", "MANAGER" },
+                    { "794ce43e-e8c2-481b-8c41-c5efd7081033", null, "Administrator", "ADMINISTRATOR" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "464e58b6-5934-4314-a04a-af4033d8a801");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "794ce43e-e8c2-481b-8c41-c5efd7081033");
+        }
+    }
+}
