06f1dae0830dcbbf5f97732e3416e4a42dd33a3d
todor-grigorov <t.grigorov99@gmail.com>
2026-02-16 21:41:29 +0100

    feat: add IAuthenticationService into Services.Abstractions

---
+++ CompanyEmployees.Core.Services.Abstractions/IAuthenticationService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Shared.DataTransferObjects;
+
+namespace CompanyEmployees.Core.Services.Abstractions
+{
+    public interface IAuthenticationService
+    {
+        Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistration);
+    }
+}
