881f8946d421ee939496bc1c0010c6a88506b194
todor-grigorov <t.grigorov99@gmail.com>
2026-02-16 21:38:44 +0100

    feat: add new AuthenticationController

---
+++ CompanyEmployees.Infrastructure.Presentation/Controllers/AuthenticationController.cs
@@ -0,0 +1,13 @@
+﻿using CompanyEmployees.Core.Services.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyEmployees.Infrastructure.Presentation.Controllers
+{
+    [Route("api/authentication")]
+    [ApiController()]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IServiceManager _service;
+        public AuthenticationController(IServiceManager service) => _service = service;
+    }
+}
