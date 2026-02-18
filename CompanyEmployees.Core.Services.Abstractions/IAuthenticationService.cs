38fb51b78189e175bc5c4ca3f215c634eb4aeddc
todor-grigorov <t.grigorov99@gmail.com>
2026-02-18 15:38:35 +0100

    feat:  add ValidateUser and CreateToken methods to IAuthenticationService

--- CompanyEmployees.Core.Services.Abstractions/IAuthenticationService.cs
+++ CompanyEmployees.Core.Services.Abstractions/IAuthenticationService.cs
@@ -6,5 +6,7 @@ namespace CompanyEmployees.Core.Services.Abstractions
     public interface IAuthenticationService
     {
         Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistration);
+        Task<bool> ValidateUser(UserForAuthenticationDto userForAuth);
+        Task<string> CreateToken();
     }
 }
