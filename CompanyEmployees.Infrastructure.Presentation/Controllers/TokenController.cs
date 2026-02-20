4701502bdcd84c4bf0725f627b94895b33020d9d
todor-grigorov <t.grigorov99@gmail.com>
2026-02-20 17:34:46 +0100

    feat: add TokenController to handle token refresh

---
+++ CompanyEmployees.Infrastructure.Presentation/Controllers/TokenController.cs
@@ -0,0 +1,13 @@
+﻿using CompanyEmployees.Core.Services.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyEmployees.Infrastructure.Presentation.Controllers
+{
+    [Route("api/token")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly IServiceManager _service;
+        public TokenController(IServiceManager service) => _service = service;
+    }
+}
