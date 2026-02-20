368660ba874dfc2e9d71131e1dc022e878ef825b
todor-grigorov <t.grigorov99@gmail.com>
2026-02-20 17:53:09 +0100

    feat: implement Refresh action in TokenController

--- CompanyEmployees.Infrastructure.Presentation/Controllers/TokenController.cs
+++ CompanyEmployees.Infrastructure.Presentation/Controllers/TokenController.cs
@@ -1,5 +1,7 @@
 ﻿using CompanyEmployees.Core.Services.Abstractions;
+using CompanyEmployees.Infrastructure.Presentation.ActionFilters;
 using Microsoft.AspNetCore.Mvc;
+using Shared.DataTransferObjects;
 
 namespace CompanyEmployees.Infrastructure.Presentation.Controllers
 {
@@ -9,5 +11,13 @@ namespace CompanyEmployees.Infrastructure.Presentation.Controllers
     {
         private readonly IServiceManager _service;
         public TokenController(IServiceManager service) => _service = service;
+
+        [HttpPost("refresh")]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
+        public async Task<IActionResult> Refresh([FromBody] TokenDto tokenDto)
+        {
+            var tokenDtoToReturn = await _service.AuthenticationService.RefreshToken(tokenDto);
+            return Ok(tokenDtoToReturn);
+        }
     }
 }
