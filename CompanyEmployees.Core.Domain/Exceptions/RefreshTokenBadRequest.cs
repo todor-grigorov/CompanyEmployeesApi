119bb611b1cd830f3bb07dafec46f4810815a157
todor-grigorov <t.grigorov99@gmail.com>
2026-02-20 17:52:26 +0100

    feat: add RefreshTokenBadRequest exception

---
+++ CompanyEmployees.Core.Domain/Exceptions/RefreshTokenBadRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace CompanyEmployees.Core.Domain.Exceptions
+{
+    public class RefreshTokenBadRequest : Exception
+    {
+        public RefreshTokenBadRequest() : base("Token could not be refreshed!")
+        {
+
+        }
+    }
+}
