13b8062347b42f8819e297af791d32d7fff2cf8a
todor-grigorov <t.grigorov99@gmail.com>
2026-03-02 21:41:21 +0100

    feat: add CustomHealthCheck class inside the CompanyEmployees (Main) project

---
+++ CompanyEmployees/CustomHealthCheck.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace CompanyEmployees
+{
+    public class CustomHealthCheck
+    {
+        private Random _random = new Random();
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context,
+            CancellationToken cancellationToken = default)
+        {
+            var responseTime = _random.Next(1, 300);
+            return await Task.FromResult(responseTime switch
+            {
+                < 100 => HealthCheckResult.Healthy("Healthy result from CustomHealthCheck"),
+                (> 100) and (< 200) => HealthCheckResult.Degraded("Degraded result from CustomHealthCheck"),
+                _ => HealthCheckResult.Unhealthy("Unhealthy result from CustomHealthCheck")
+            });
+        }
+    }
+}
