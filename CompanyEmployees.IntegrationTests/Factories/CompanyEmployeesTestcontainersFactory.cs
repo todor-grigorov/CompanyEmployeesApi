857d8dee63c556602d8afd259befd61675d16ba8
todor-grigorov <t.grigorov99@gmail.com>
2026-03-08 22:24:11 +0100

    feat: overriding the Database Configuration (Test project)

--- CompanyEmployees.IntegrationTests/Factories/CompanyEmployeesTestcontainersFactory.cs
+++ CompanyEmployees.IntegrationTests/Factories/CompanyEmployeesTestcontainersFactory.cs
@@ -1,11 +1,24 @@
-﻿using DotNet.Testcontainers.Builders;
+﻿using CompanyEmployees.Infrastructure.Persistence;
+using DotNet.Testcontainers.Builders;
 using DotNet.Testcontainers.Containers;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace CompanyEmployees.IntegrationTests.Factories
 {
     public class CompanyEmployeesTestcontainersFactory : WebApplicationFactory<Program>
     {
+        private const string Database = "master";
+        private const string Username = "sa";
+        private const string Password = "yourStrong(!)Password";
+        private const ushort PostgresPort = 5432;
+
+        private readonly IContainer _postgresContainer;
+
         public CompanyEmployeesTestcontainersFactory()
         {
             //_postgresContainer = new PostgreSqlBuilder().Build();
@@ -19,12 +32,32 @@ namespace CompanyEmployees.IntegrationTests.Factories
                 .Build();
         }
 
-        private const string Database = "master";
-        private const string Username = "sa";
-        private const string Password = "yourStrong(!)Password";
-        private const ushort PostgresPort = 5432;
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            var host = _postgresContainer.Hostname;
+            var port = _postgresContainer.GetMappedPublicPort(PostgresPort);
+            builder.ConfigureServices(services =>
+            {
+                services.RemoveAll(typeof(DbContextOptions<RepositoryContext>));
 
-        private readonly IContainer _postgresContainer;
+                services.AddDbContext<RepositoryContext>(options =>
+                    options.UseNpgsql($"Host={host};Port={port};Database={Database};Username={Username};Password={Password}")
+                        .ConfigureWarnings(warnings => warnings.Ignore(RelationalEventId.PendingModelChangesWarning)));
+
+                var sp = services.BuildServiceProvider();
+                using var scope = sp.CreateScope();
+                var appContext = scope.ServiceProvider.GetRequiredService<RepositoryContext>();
+                try
+                {
+                    appContext.Database.Migrate(); // Apply all migrations to keep schema in sync
+                }
+                catch (Exception ex)
+                {
+                    // Log or handle migration exceptions if needed
+                    throw;
+                }
+            });
+        }
     }
     public partial class Program { }
 }
