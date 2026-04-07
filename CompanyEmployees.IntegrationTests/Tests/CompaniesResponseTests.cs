3d5bb622844b7a08fd93517b5118dd5a04346716
todor-grigorov <t.grigorov99@gmail.com>
2026-04-08 01:04:47 +0200

    feat: init setup of CompaniesResponseTests

---
+++ CompanyEmployees.IntegrationTests/Tests/CompaniesResponseTests.cs
@@ -0,0 +1,15 @@
+﻿using CompanyEmployees.IntegrationTests.Factories;
+
+namespace CompanyEmployees.IntegrationTests.Tests
+{
+    public class CompaniesResponseTests : IClassFixture<CompanyEmployeesTestcontainersFactory>
+    {
+        private readonly HttpClient _client;
+        private const string CompaniesUrl = "/api/companies";
+
+        public CompaniesResponseTests(CompanyEmployeesTestcontainersFactory factory)
+        {
+            _client = factory.CreateClient();
+        }
+    }
+}
