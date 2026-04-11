b2cfea9d82ee460c987bbddd9895857b22788bd1
todor-grigorov <t.grigorov99@gmail.com>
2026-04-12 00:10:27 +0200

    test: 404 not found

--- CompanyEmployees.IntegrationTests/Tests/ValidationTests.cs
+++ CompanyEmployees.IntegrationTests/Tests/ValidationTests.cs
@@ -1,4 +1,5 @@
 ﻿using CompanyEmployees.IntegrationTests.Factories;
+using System.Net;
 
 namespace CompanyEmployees.IntegrationTests.Tests
 {
@@ -10,6 +11,16 @@ namespace CompanyEmployees.IntegrationTests.Tests
         public ValidationTests(CompanyEmployeesTestcontainersFactory factory)
         {
             _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task WhenEntityDoesntExist_ThenReturns404NotFound()
+        {
+            // Act
+            var response = await _client.GetAsync($"{CompaniesUrl}/d9d4c053-49b6-410c-bc78-2d54a9991870");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
         }
     }
 }
