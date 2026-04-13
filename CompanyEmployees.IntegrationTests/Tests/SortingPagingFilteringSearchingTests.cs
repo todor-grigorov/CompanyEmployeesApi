7ed8f042b82b2059fe29167ba6c924f315218ca2
todor-grigorov <t.grigorov99@gmail.com>
2026-04-13 20:37:19 +0200

    feat: setup SortingPagingFilteringSearchingTests class

---
+++ CompanyEmployees.IntegrationTests/Tests/SortingPagingFilteringSearchingTests.cs
@@ -0,0 +1,15 @@
+﻿using CompanyEmployees.IntegrationTests.Factories;
+
+namespace CompanyEmployees.IntegrationTests.Tests
+{
+    public class SortingPagingFilteringSearchingTests : IClassFixture<CompanyEmployeesTestcontainersFactory>
+    {
+        private readonly HttpClient _client;
+        private const string EmployeesUrl = "/api/companies";
+
+        public SortingPagingFilteringSearchingTests(CompanyEmployeesTestcontainersFactory factory)
+        {
+            _client = factory.CreateClient();
+        }
+    }
+}
