85df277670023ddf5814a364bc5230ad219fadf0
todor-grigorov <t.grigorov99@gmail.com>
2026-04-13 20:09:09 +0200

    feat: init setup of AuthTests class

---
+++ CompanyEmployees.IntegrationTests/Tests/AuthTests.cs
@@ -0,0 +1,16 @@
+﻿using CompanyEmployees.IntegrationTests.Factories;
+
+namespace CompanyEmployees.IntegrationTests.Tests
+{
+    public class AuthTests : IClassFixture<CompanyEmployeesTestcontainersFactory>
+    {
+        private readonly HttpClient _client;
+        private const string AuthUrl = "/api/authentication";
+        private const string CompaniesUrl = "/api/companies";
+
+        public AuthTests(CompanyEmployeesTestcontainersFactory factory)
+        {
+            _client = factory.CreateClient();
+        }
+    }
+}
