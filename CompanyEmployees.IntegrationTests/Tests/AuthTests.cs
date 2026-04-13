455d819190f508bbc2735481e01f296f4c233a1a
todor-grigorov <t.grigorov99@gmail.com>
2026-04-13 20:25:04 +0200

    test: user login

--- CompanyEmployees.IntegrationTests/Tests/AuthTests.cs
+++ CompanyEmployees.IntegrationTests/Tests/AuthTests.cs
@@ -38,5 +38,38 @@ namespace CompanyEmployees.IntegrationTests.Tests
             response.EnsureSuccessStatusCode(); // Status Code 200-299
             Assert.Equal(HttpStatusCode.Created, response.StatusCode);
         }
+
+        [Fact]
+        public async Task WhenUserLogin_ThenReturnsOkResultAndTokens()
+        {
+            // Arrange
+            var userForRegistration = new UserForRegistrationDto
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                UserName = "jdoe2",
+                Email = "contact2@jdoe.com",
+                Password = "YouWillNeverGuess_1234",
+                PhoneNumber = "1234567890",
+                Roles = ["Manager"]
+            };
+
+            var user = new UserForAuthenticationDto
+            {
+                UserName = "jdoe2",
+                Password = "YouWillNeverGuess_1234"
+            };
+
+            // Act
+            var responseRegistration = await _client.PostAsJsonAsync(AuthUrl, userForRegistration);
+            var responseAuthentication = await _client.PostAsJsonAsync($"{AuthUrl}/login", user);
+            var result = await responseAuthentication.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Created, responseRegistration.StatusCode);
+            Assert.Equal(HttpStatusCode.OK, responseAuthentication.StatusCode);
+            Assert.Contains("accessToken", result);
+            Assert.Contains("refreshToken", result);
+        }
     }
 }
