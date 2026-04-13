8bac7b99d0f0ce9c315ffe2b8bf8f2566f8c81b8
todor-grigorov <t.grigorov99@gmail.com>
2026-04-13 20:11:00 +0200

    test: add ueser registartion test

--- CompanyEmployees.IntegrationTests/Tests/AuthTests.cs
+++ CompanyEmployees.IntegrationTests/Tests/AuthTests.cs
@@ -1,4 +1,7 @@
 ﻿using CompanyEmployees.IntegrationTests.Factories;
+using Shared.DataTransferObjects;
+using System.Net;
+using System.Net.Http.Json;
 
 namespace CompanyEmployees.IntegrationTests.Tests
 {
@@ -11,6 +14,29 @@ namespace CompanyEmployees.IntegrationTests.Tests
         public AuthTests(CompanyEmployeesTestcontainersFactory factory)
         {
             _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task WhenUserRegistration_ThenReturnsCreated()
+        {
+            // Arrange
+            var userForRegistration = new UserForRegistrationDto
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                UserName = "jdoe",
+                Email = "contact@jdoe.com",
+                Password = "YouWillNeverGuess_1234",
+                PhoneNumber = "1234567890",
+                Roles = ["Manager"]
+            };
+
+            // Act
+            var response = await _client.PostAsJsonAsync(AuthUrl, userForRegistration);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
         }
     }
 }
