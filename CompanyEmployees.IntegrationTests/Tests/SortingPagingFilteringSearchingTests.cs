6099827ced12b45185a8b39546a4c3745aa18930
todor-grigorov <t.grigorov99@gmail.com>
2026-04-14 15:25:44 +0200

    test: get all employees with custom Accept reuqest header

--- CompanyEmployees.IntegrationTests/Tests/SortingPagingFilteringSearchingTests.cs
+++ CompanyEmployees.IntegrationTests/Tests/SortingPagingFilteringSearchingTests.cs
@@ -1,4 +1,7 @@
-﻿using CompanyEmployees.IntegrationTests.Factories;
+﻿using CompanyEmployees.Core.Domain.Entities;
+using CompanyEmployees.IntegrationTests.Factories;
+using System.Net;
+using System.Net.Http.Json;
 
 namespace CompanyEmployees.IntegrationTests.Tests
 {
@@ -10,6 +13,24 @@ namespace CompanyEmployees.IntegrationTests.Tests
         public SortingPagingFilteringSearchingTests(CompanyEmployeesTestcontainersFactory factory)
         {
             _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task WhenGetEndpointsRequested_ThenReturnsOKAndEntities()
+        {
+            // Arrange
+            _client.DefaultRequestHeaders.Add("Accept", "application/tg.code.apiroot+json");
+
+            // Act
+            var response = await _client.GetAsync($"{EmployeesUrl}/C9D4C053-49B6-410C-BC78-2D54A9991870/employees");
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+
+            var result = await response.Content.ReadFromJsonAsync<List<Entity>>();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(2, result?.Count);
+            Assert.True(response.Headers.Contains("X-Pagination"));
         }
     }
 }
