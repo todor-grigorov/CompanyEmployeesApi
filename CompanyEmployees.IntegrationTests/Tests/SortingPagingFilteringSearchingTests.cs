2a6b62b741329132494ae6084720b74ffb45d739
todor-grigorov <t.grigorov99@gmail.com>
2026-04-14 15:29:53 +0200

    test: get employees using page number and page size

--- CompanyEmployees.IntegrationTests/Tests/SortingPagingFilteringSearchingTests.cs
+++ CompanyEmployees.IntegrationTests/Tests/SortingPagingFilteringSearchingTests.cs
@@ -1,5 +1,6 @@
 ﻿using CompanyEmployees.Core.Domain.Entities;
 using CompanyEmployees.IntegrationTests.Factories;
+using Microsoft.AspNetCore.WebUtilities;
 using System.Net;
 using System.Net.Http.Json;
 
@@ -30,6 +31,32 @@ namespace CompanyEmployees.IntegrationTests.Tests
             var result = await response.Content.ReadFromJsonAsync<List<Entity>>();
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
             Assert.Equal(2, result?.Count);
+            Assert.True(response.Headers.Contains("X-Pagination"));
+        }
+
+        [Fact]
+        public async Task WhenPaging_ThenReturnsOKAndEntity()
+        {
+            // Arrange
+            _client.DefaultRequestHeaders.Add("Accept", "application/tg.code.apiroot+json");
+
+            var url = $"{EmployeesUrl}/C9D4C053-49B6-410C-BC78-2D54A9991870/employees";
+            var queryParams = new Dictionary<string, string>
+            {
+                ["pageNumber"] = "2",
+                ["pageSize"] = "1",
+            };
+            var urlWithParams = QueryHelpers.AddQueryString(url, queryParams);
+
+            // Act
+            var response = await _client.GetAsync(urlWithParams);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+
+            var result = await response.Content.ReadFromJsonAsync<List<Entity>>();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(1, result?.Count);
             Assert.True(response.Headers.Contains("X-Pagination"));
         }
     }
